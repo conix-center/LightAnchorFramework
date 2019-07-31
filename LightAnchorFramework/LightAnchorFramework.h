9a8d853c413ad069336e969d84d2878590307d86
Nick Wilkerson <nickwilkerson@gmail.com>
2019-07-31 16:18:01 -0400

    Initial Commit

---
+++ LightAnchorFramework/LightAnchorFramework.h
@@ -0,0 +1,19 @@
+//
+//  LightAnchorFramework.h
+//  LightAnchorFramework
+//
+//  Created by Nick Wilkerson on 7/31/19.
+//  Copyright © 2019 Wiselab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LightAnchorFramework.
+FOUNDATION_EXPORT double LightAnchorFrameworkVersionNumber;
+
+//! Project version string for LightAnchorFramework.
+FOUNDATION_EXPORT const unsigned char LightAnchorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightAnchorFramework/PublicHeader.h>
+
+
